2e003c29fd4e9815f19d8b0ee030585f0265394b
Beatriz Souza <beatriz_silva_souza@yahoo.com.br>
2024-10-01 20:04:58 -0300

    Inicio curso de Spring

---
+++ src/main/java/br/com/alura/Screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,29 @@
+package br.com.alura.Screenmatch;
+
+import br.com.alura.Screenmatch.model.DadosSerie;
+import br.com.alura.Screenmatch.service.ConsumoAPI;
+import br.com.alura.Screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLOutput;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		ConsumoAPI consumoApi = new ConsumoAPI();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+
+	}
+}
